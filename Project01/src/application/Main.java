646582bdebf8fe0d2ec9425f7fbeccd6ab3d93e3
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-06-22 17:06:21 +0600

    Added Introduction.fxml and working on UI

--- Project01/src/application/Main.java
+++ Project01/src/application/Main.java
@@ -1,24 +1,37 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	private StackPane mainlayout;
+	private Stage primaryStage;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+		try {	
+			this.primaryStage = primaryStage;
 			primaryStage.setTitle("Resume Generator");
-			primaryStage.show();
+			showIntroductionView();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void showIntroductionView() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/Introduction.fxml"));
+		mainlayout = loader.load();
+		Scene scene = new Scene(mainlayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
