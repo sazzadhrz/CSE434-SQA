ca89101876d4850b8571bc7d4d4614ba290f8f3f
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-06-25 00:01:06 +0600

    added a method o inject data into html

--- Project01/src/application/Main.java
+++ Project01/src/application/Main.java
@@ -1,6 +1,11 @@
 package application;
 	
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -34,7 +39,29 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 	
+	public static void baal() throws IOException {
+		File htmlTemplateFile = new File("html/template.html");
+		String htmlString = FileUtils.readFileToString(htmlTemplateFile);
+		System.out.println(htmlString);
+		String title = "New Page";
+		String body = "This is Body";
+		htmlString = htmlString.replace("$title", title);
+		htmlString = htmlString.replace("$body", body);
+		System.out.println(htmlString);
+		
+		File outfile = new File("html/output.html");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(outfile));
+		bw.write(htmlString);
+		bw.close();
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
+		try {
+			baal();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
