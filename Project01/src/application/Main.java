43b618eaa7a2d30595b0556553828eab084bf0ab
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-07-16 00:23:30 +0600

    added name validation method

--- Project01/src/application/Main.java
+++ Project01/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -12,25 +12,24 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	public static ArrayList<String> arraylist = new ArrayList<String>();
-	
+
 	private StackPane mainlayout;
-	Stage primaryStage;
-	
+	private Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) {
-		try {	
+		try {
 			this.primaryStage = primaryStage;
 			primaryStage.setTitle("Resume Generator");
 			showIntroductionView();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void showIntroductionView() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("view/Introduction.fxml"));
@@ -39,68 +38,60 @@ public class Main extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
-	
-	/*private static void generateHeader() throws IOException {
-		String head = "<head>"
-				+ "<title>Resume</title>"
-				+ "</head>";
-		
-		File outfile = new File("html/output.html");
-		BufferedWriter bw = new BufferedWriter(new FileWriter(outfile));
-		bw.write(head);
-		bw.append("appended");
-		bw.close();
-	}
-	
-	
-	
-	public static void overwriteTemplateAndGenerateCV() throws IOException {
-		File htmlTemplateFile = new File("html/template.html");
-		String htmlString = FileUtils.readFileToString(htmlTemplateFile);
-//		System.out.println(htmlString);
-		String title = "New Page";
-		String body = "This is Body";
-		htmlString = htmlString.replace("$title", title);
-		htmlString = htmlString.replace("$body", body);
-//		System.out.println(htmlString);
-		
-		generateCVinHTML(htmlString);
-		
-	}
-	
-	// create a new html file and overwrite the template.html
-	public static void generateCVinHTML (String htmlString) throws IOException {
-		File outfile = new File("html/output.html");
-		BufferedWriter bw = new BufferedWriter(new FileWriter(outfile));
-		bw.write(htmlString);
-		bw.close();
-	} */
-	
+
+	/*
+	 * private static void generateHeader() throws IOException { String head =
+	 * "<head>" + "<title>Resume</title>" + "</head>";
+	 * 
+	 * File outfile = new File("html/output.html"); BufferedWriter bw = new
+	 * BufferedWriter(new FileWriter(outfile)); bw.write(head);
+	 * bw.append("appended"); bw.close(); }
+	 * 
+	 * 
+	 * 
+	 * public static void overwriteTemplateAndGenerateCV() throws IOException {
+	 * File htmlTemplateFile = new File("html/template.html"); String htmlString
+	 * = FileUtils.readFileToString(htmlTemplateFile); //
+	 * System.out.println(htmlString); String title = "New Page"; String body =
+	 * "This is Body"; htmlString = htmlString.replace("$title", title);
+	 * htmlString = htmlString.replace("$body", body); //
+	 * System.out.println(htmlString);
+	 * 
+	 * generateCVinHTML(htmlString);
+	 * 
+	 * }
+	 * 
+	 * // create a new html file and overwrite the template.html public static
+	 * void generateCVinHTML (String htmlString) throws IOException { File
+	 * outfile = new File("html/output.html"); BufferedWriter bw = new
+	 * BufferedWriter(new FileWriter(outfile)); bw.write(htmlString);
+	 * bw.close(); }
+	 */
+
 	public static void main(String[] args) throws IOException {
 		launch(args);
-		
+
 		generateHTML();
-		
-//		overwriteTemplateAndGenerateCV();
-	} 
-	
+
+		// overwriteTemplateAndGenerateCV();
+	}
+
 	public static void generateHTML() throws IOException {
 		arraylist.add(bodyClose());
-		
+
 		File outfile = new File("html/output.html");
 		BufferedWriter bw = new BufferedWriter(new FileWriter(outfile));
-		
-		for (int i=0; i<arraylist.size(); i++) {
+
+		for (int i = 0; i < arraylist.size(); i++) {
 			bw.append(arraylist.get(i));
 			bw.newLine();
 		}
-		
+
 		bw.close();
 	}
-	
+
 	private static String bodyClose() {
-		return "</body>";	
-	} 
-	
+		return "</body>";
+	}
+
 }
--- Project01/src/application/controller/IntroductionController.java
+++ Project01/src/application/controller/IntroductionController.java
@@ -4,65 +4,83 @@ import application.Main;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-
 public class IntroductionController {
-	@FXML Button introductionNextBtn;
-	@FXML Button introductionCancelBtn;
-	@FXML TextField nameTF;
-	@FXML TextField addressTF;
-	@FXML TextField emailTF;
-	@FXML TextField phoneTF;
-	
-	//Next button controller
+	@FXML
+	Button introductionNextBtn;
+	@FXML
+	Button introductionCancelBtn;
+	@FXML
+	TextField nameTF;
+	@FXML
+	TextField addressTF;
+	@FXML
+	TextField emailTF;
+	@FXML
+	TextField phoneTF;
+	@FXML
+	Label warningLabel;
+
+	// Cancel button controller
+	public void introductionCancelBtnActionController() {
+		System.out.println("Cancel btn pressed");
+
+		Platform.exit();
+		System.exit(0);
+	}
+
+	// Next button controller
 	public void introductionNextBtnActionController() {
 		System.out.println("next btn pressed");
-		
+
 		Main.arraylist.add(generateHeader());
 		Main.arraylist.add(bodyOpen());
 		Main.arraylist.add(generateIntroHtml());
-//		System.out.println(Main.arraylist);
+		// System.out.println(Main.arraylist);
+
+		nameFieldValidation();
 	}
-	
-	//Cancel button controller
-	public void introductionCancelBtnActionController() {
-		System.out.println("Cancel btn pressed");
-		
-		Platform.exit();
-        System.exit(0);
+
+	public boolean nameFieldValidation() {
+		if (nameTF.getText() != "null") {
+			warningLabel.setText("*Warning!");
+			System.out.println("False");
+			return false;
+		}
+
+		else {
+			System.out.print(nameTF.getText());
+			System.out.println("a");
+			System.out.println("True");
+			return true;
+		}
+
 	}
-	
+
 	// HTML tags as method
 	private String h5(String info) {
 		return "<h5>" + info + "</h5>" + "\n";
 	}
-	
+
 	// HTML tags as method
 	private String h2(String info) {
 		return "<h2>" + info + "</h2>" + "\n";
 	}
-	
+
 	private String generateIntroHtml() {
-		return 
-		"<div class=\"introduction\">" + "\n" +
-		h2(nameTF.getText()) +
-		h5(addressTF.getText()) +
-		h5(emailTF.getText()) +
-		h5(phoneTF.getText()) +
-		"</div>";
+		return "<div class=\"introduction\">" + "\n" + h2(nameTF.getText()) + h5(addressTF.getText())
+				+ h5(emailTF.getText()) + h5(phoneTF.getText()) + "</div>";
 	}
-	
+
 	private String generateHeader() {
-		String head = "<head>" + "\n"  
-				+ "<title>Resume of "+ nameTF.getText() + "</title>" + "\n"
-				+ "</head>";
+		String head = "<head>" + "\n" + "<title>Resume of " + nameTF.getText() + "</title>" + "\n" + "</head>";
 		return head;
 	}
-	
+
 	private String bodyOpen() {
-		return "<body>";	
+		return "<body>";
 	}
-	
-	
+
 }
