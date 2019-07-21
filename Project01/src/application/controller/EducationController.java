2cd1c9a4d0be742cb9895cc13693b6175a116553
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-07-21 23:15:11 +0600

    updated EducationController

--- Project01/src/application/controller/EducationController.java
+++ Project01/src/application/controller/EducationController.java
@@ -1,5 +1,37 @@
 package application.controller;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.Label;
+
 public class EducationController {
+	@FXML
+	private TextField name1;
+	@FXML
+	private TextField degree1;
+	@FXML
+	private TextField year1;
+	@FXML
+	private TextField cgpa1;
+	@FXML
+	private TextField name2;
+	@FXML
+	private TextField degree2;
+	@FXML
+	private TextField year2;
+	@FXML
+	private TextField cgpa2;
+	@FXML
+	private Label warningLabel;
+	@FXML
+	private Button back;
+	@FXML
+	private Button cancel;
+	@FXML
+	private Button next;
 
 }
