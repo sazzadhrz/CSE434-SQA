1566163af08d8d8fe450f16d9850123a5ae048ca
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-07-21 23:59:26 +0600

    working on controller

--- Project01/src/application/controller/EducationController.java
+++ Project01/src/application/controller/EducationController.java
@@ -1,13 +1,19 @@
 package application.controller;
 
+import java.io.IOException;
+
+import application.Main;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
-import javafx.scene.control.TextField;
-
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 
 public class EducationController {
 	@FXML
@@ -35,11 +41,20 @@ public class EducationController {
 	@FXML
 	private Button next;
 	
+	
 	public void educationCancelBtnActionController() {
 		System.out.println("Cancel btn pressed");
 
 		Platform.exit();
 		System.exit(0);
+	}
+	
+	public void educationNextBtnActionController() {
+		
+	}
+	
+	public void educationBackBtnActionController() throws IOException {
+		
 	}
 
 }
