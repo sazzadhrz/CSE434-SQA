7a1adb06ef3143555ff75055961231c018fdacc7
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-07-26 00:07:20 +0600

    cleaned the code

--- Project01/src/application/Main.java
+++ Project01/src/application/Main.java
@@ -39,41 +39,12 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 
-	/*
-	 * private static void generateHeader() throws IOException { String head =
-	 * "<head>" + "<title>Resume</title>" + "</head>";
-	 * 
-	 * File outfile = new File("html/output.html"); BufferedWriter bw = new
-	 * BufferedWriter(new FileWriter(outfile)); bw.write(head);
-	 * bw.append("appended"); bw.close(); }
-	 * 
-	 * 
-	 * 
-	 * public static void overwriteTemplateAndGenerateCV() throws IOException {
-	 * File htmlTemplateFile = new File("html/template.html"); String htmlString
-	 * = FileUtils.readFileToString(htmlTemplateFile); //
-	 * System.out.println(htmlString); String title = "New Page"; String body =
-	 * "This is Body"; htmlString = htmlString.replace("$title", title);
-	 * htmlString = htmlString.replace("$body", body); //
-	 * System.out.println(htmlString);
-	 * 
-	 * generateCVinHTML(htmlString);
-	 * 
-	 * }
-	 * 
-	 * // create a new html file and overwrite the template.html public static
-	 * void generateCVinHTML (String htmlString) throws IOException { File
-	 * outfile = new File("html/output.html"); BufferedWriter bw = new
-	 * BufferedWriter(new FileWriter(outfile)); bw.write(htmlString);
-	 * bw.close(); }
-	 */
-
+	
 	public static void main(String[] args) throws IOException {
 		launch(args);
 
 		generateHTML();
 
-		// overwriteTemplateAndGenerateCV();
 	}
 
 	public static void generateHTML() throws IOException {
