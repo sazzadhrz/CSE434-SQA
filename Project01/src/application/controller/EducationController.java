44d4ce82b5bd0a46edb46564ee4eccba0757608f
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-07-26 23:51:23 +0600

    education name field validation added

--- Project01/src/application/controller/EducationController.java
+++ Project01/src/application/controller/EducationController.java
@@ -42,6 +42,21 @@ public class EducationController {
 	@FXML
 	private VBox educationlayout;
 	
+	public boolean name1FieldValidation() {
+		if (name1.getText() == "null" || name1.getText().trim().isEmpty()) {
+			warningLabel.setText("*Warning! All of the fields must be filled up");
+			System.out.println("False, name null");
+			return false;
+		}
+
+		else {
+			System.out.println(name1.getText());
+			System.out.println("True, name not null");
+			return true;
+		}
+
+	}
+	
 	
 	public void educationCancelBtnActionController() {
 		System.out.println("Cancel btn pressed");
