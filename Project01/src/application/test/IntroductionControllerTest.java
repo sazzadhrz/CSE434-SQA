3a6d75e3fdfe41e238451b36792b3e2418acf883
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-07-27 20:44:03 +0600

    added test case for introduction controller

---
+++ Project01/src/application/test/IntroductionControllerTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package application.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Sazzad
+ *
+ */
+public class IntroductionControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
