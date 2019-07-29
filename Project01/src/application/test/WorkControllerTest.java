0cd6cabfa5897181069d0997e5eb4902e79b49ad
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-07-30 00:36:06 +0600

    added ISP in WorkContoller isNull Test

--- Project01/src/application/test/WorkControllerTest.java
+++ Project01/src/application/test/WorkControllerTest.java
@@ -34,9 +34,47 @@ public class WorkControllerTest {
 		wc = null;
 	}
 
+	
+	
+	/*
+	 * INPUT SPACE PARTITIONING FOR isNull
+	 * 
+	 * PARAMETER STRING
+	 * DOMAIN - ANY KING OF STRING
+	 * 
+	 * CHARACTERISTICS 
+	 * 1 - NULL
+	 * 2 - NOT NULL
+	 * 
+	 * BLOCK
+	 * 1 - ESCAPE SEQUENCE (NULL, WHITESPACE, TAB, NEW LINE ETC.)
+	 * 2 - ESCAPE SEQUENCE EXCEPT ( \", \', \\)
+	 * 3 - ANYTHING LENGTH > 0
+	 *  
+	 */
+	
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	public void IsNullTest() {
+		// BLOCK 1
+		assertTrue(wc.isNull(""));
+		assertTrue(wc.isNull(" \t"));
+		assertTrue(wc.isNull("\n"));
+		assertTrue(wc.isNull("                "));
+		assertTrue(wc.isNull("\b"));
+		assertTrue(wc.isNull("\f"));
+		assertTrue(wc.isNull("\r"));
+		
+		// BLOCK 2
+		assertFalse(wc.isNull("\\"));
+		assertFalse(wc.isNull("\""));
+		assertFalse(wc.isNull("\'"));
+		
+		// BLOCK 3
+		assertFalse(wc.isNull("Supra"));
+		assertFalse(wc.isNull("545"));
+		assertFalse(wc.isNull("saz5"));
+		assertFalse(wc.isNull("&$^#*&^#&"));
+		assertFalse(wc.isNull("M1x3d f$C@"));
 	}
 
 }
