51c7478930b1792f04493fccff0ce6f6fbbd3ba5
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-18 23:43:34 +0600

    opening browser using selenium

--- Project02/src/application/SeleniumTest.java
+++ Project02/src/application/SeleniumTest.java
@@ -1,8 +1,18 @@
 package application;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class SeleniumTest {
 	
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+		           "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
 		
+		WebDriver driver = new ChromeDriver();
+//		WebDriver d = new EdgeDriver();
+		
+		driver.get("https://seleniumhq.org/");
 	}
 }
