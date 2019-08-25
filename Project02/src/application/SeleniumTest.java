38930e6969e06931380a9d5db8ad0ffdffb0dbb2
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-25 23:39:30 +0600

    added delay to visualize

--- Project02/src/application/SeleniumTest.java
+++ Project02/src/application/SeleniumTest.java
@@ -7,14 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
-		           "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+				"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
 		
 		WebDriver driver = new ChromeDriver();
 //		WebDriver d = new EdgeDriver();
 		
 		driver.get("https://seleniumhq.org/");
+		driver.wait(3000);
 		
         driver.manage().window().maximize();  
         
