ae65f129999ec775451763fd30483a32c56b33ce
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-26 00:36:17 +0600

    upgraded to version 3.141.59 and started testing phptravels.net

--- Project02/src/application/SeleniumTest.java
+++ Project02/src/application/SeleniumTest.java
@@ -1,7 +1,7 @@
 package application;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,20 +9,18 @@ public class SeleniumTest {
 	
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
-				"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
+				"F:/chromedriver_win32/chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
-//		WebDriver d = new EdgeDriver();
-		
-		driver.get("https://seleniumhq.org/");
-		driver.wait(3000);
-		
-        driver.manage().window().maximize();  
-        
-        //Scroll down the webpage by 5000 pixels  
-      JavascriptExecutor js = (JavascriptExecutor)driver;  
-      js.executeScript("scrollBy(0, 5000)");  
-      
-      driver.findElement(By.linkText("Documentation")).click();  
+	      
+		// Launch website  
+		    driver.navigate().to("https://www.phptravels.net/");  
+		          
+		    // Click on the search text box and send value  
+		    driver.findElement(By.linkText("OFFERS")).click();
+		    
+		          
+		    // Click on the search button  
+		    //driver.findElement(By.name("btnI")).click();  
 	}
 }
