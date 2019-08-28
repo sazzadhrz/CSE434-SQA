c03918533e98d6c4b2d8a4979fb60c6a99f561fc
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-29 00:11:51 +0600

    added noConfirmPasswordTest

--- Project02/src/application/SignUpTest.java
+++ Project02/src/application/SignUpTest.java
@@ -105,6 +105,20 @@ public class SignUpTest {
 
 	}
 	
+	@Test 
+	public void noConfirmPasswordTest() {
+		driver.findElement(By.name("firstname")).sendKeys("Sazzad");
+		driver.findElement(By.name("lastname")).sendKeys("Hossain");
+		driver.findElement(By.name("phone")).sendKeys("01613645555");
+		driver.findElement(By.name("email")).sendKeys("sazzad.hossian09@northsouth.edu");
+		driver.findElement(By.name("password")).sendKeys("haha123");
+		
+		driver.findElement(By.className("signupbtn")).click();
+		
+		driver.findElement(By.className("resultsignup")).getText();
+
+	}
+	
 	
 	
 	@AfterMethod
