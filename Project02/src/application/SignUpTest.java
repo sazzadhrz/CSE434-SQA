105ab17d4a4f68283029440f60d8b1d091df36bf
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-28 23:54:41 +0600

    added no inputTest

--- Project02/src/application/SignUpTest.java
+++ Project02/src/application/SignUpTest.java
@@ -1,9 +1,11 @@
 package application;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class SignUpTest {
 	
@@ -19,6 +21,11 @@ public class SignUpTest {
 		driver.manage().window().maximize();
 		driver.navigate().to("https://www.phptravels.net/register"); 
 		
+	}
+	
+	@Test 
+	public void noInputTest() {
+		driver.findElement(By.className("signupbtn")).click();
 	}
 	
 	
