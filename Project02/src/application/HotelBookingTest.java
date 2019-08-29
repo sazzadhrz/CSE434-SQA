11a05ef64d31a9455a3d50715cbd51568a4657ad
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-30 01:27:14 +0600

    finished hotelbookingTest

--- Project02/src/application/HotelBookingTest.java
+++ Project02/src/application/HotelBookingTest.java
@@ -32,7 +32,7 @@ public class HotelBookingTest {
 	
 	@Test
 	public void searchHotel() throws InterruptedException {
-		driver.findElement(By.xpath("//*[@id='body-section']/section/div[2]/div/div/div[2]/ul/li[2]/a")).click();
+		//driver.findElement(By.xpath("//*[@id='body-section']/section/div[2]/div/div/div[2]/ul/li[2]/a")).click();
 		WebElement searchbox = driver.findElement(By.xpath("//*[@id='s2id_location']/a"));
 		
 		System.out.println(searchbox.getText());
@@ -43,12 +43,12 @@ public class HotelBookingTest {
 
 		WebElement dateFrom = driver.findElement(By.xpath("//*[@id='dpd1']/div/input"));
 		dateFrom.click();
-		dateFrom.sendKeys("31/08/2019");
+		dateFrom.sendKeys("04/09/2019");
 		dateFrom.sendKeys(Keys.ENTER);
 		
 		WebElement dateTo = driver.findElement(By.xpath("//*[@id='dpd2']/div/input"));
 		dateTo.click();
-		dateTo.sendKeys("01/09/2019");
+		dateTo.sendKeys("07/09/2019");
 		dateTo.sendKeys(Keys.ENTER);
 
 	}
@@ -56,19 +56,31 @@ public class HotelBookingTest {
 	@Test (priority = 1)
 	public void selectHotel() throws InterruptedException {
 		Thread.sleep(1000);
-		WebElement hotelSarina = driver.findElement(By.xpath("//*[@id='listing']/tbody/tr[1]/td/div[2]/div/h4/a"));
-		System.out.println(hotelSarina.getText());
-		hotelSarina.click();
+		WebElement hotel = driver.findElement(By.xpath("//*[@id='listing']/tbody/tr[1]/td/div[2]/div/h4/a"));
+		System.out.println(hotel.getText());
+		hotel.click();
 	}
 	
 	@Test (priority = 2)
 	public void bookHotel() throws InterruptedException {
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//*[@id='rooms']/div[2]/div/div/form/div/div[1]/input")).click();
-		driver.findElement(By.xpath("//*[@id='rooms']/div[2]/div/div/form/div/div[1]/input")).sendKeys("31/08/2019");
 		
-		driver.findElement(By.xpath("//*[@id='rooms']/div[2]/div/div/form/div/div[2]/input")).click();
-		driver.findElement(By.xpath("//*[@id='rooms']/div[2]/div/div/form/div/div[2]/input")).sendKeys("01/09/2019");
+		js = (JavascriptExecutor)driver;
+		js.executeScript("scroll(0, 300);");
+		
+		Thread.sleep(500);
+		
+		WebElement checkIn = driver.findElement(By.xpath("//*[@id='rooms']/div[2]/div/div/form/div/div[1]/input"));
+		WebElement checkOut = driver.findElement(By.xpath("//*[@id='rooms']/div[2]/div/div/form/div/div[2]/input"));
+		
+		checkIn.clear();
+		checkOut.clear();
+		
+		checkIn.click();
+		checkIn.sendKeys("2019-09-04");
+		
+		checkOut.click();
+		checkOut.sendKeys("2019-09-07");
 		
 		driver.findElement(By.xpath("//*[@id='rooms']/div[2]/div/div/form/div/div[5]/button")).click();
 	}
