00cfd7d8efb3e98f939b7e5b16c5e56a8282bccd
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-29 22:04:15 +0600

    added BeforeClass invokeBrowser method

--- Project02/src/application/SignInTest.java
+++ Project02/src/application/SignInTest.java
@@ -1,7 +1,23 @@
 package application;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
 public class SignInTest {
+	WebDriver driver;
+	JavascriptExecutor js;
 	
+	@BeforeClass
+	public void invokeBrowser() {
+		System.setProperty("webdriver.chrome.driver", "F:/chromedriver_win32/chromedriver.exe");
+		
+		driver = new ChromeDriver();	
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.phptravels.net/login"); 
+	}
+
 	
 	
 }
