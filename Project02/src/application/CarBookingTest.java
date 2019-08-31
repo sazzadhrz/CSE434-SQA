53b63ad34cf7476d197d16ee12a4c35e7d61ea51
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-31 17:14:35 +0600

    added searchTest method

--- Project02/src/application/CarBookingTest.java
+++ Project02/src/application/CarBookingTest.java
@@ -2,12 +2,15 @@ package application;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class CarBookingTest {
 	WebDriver driver;
@@ -31,6 +34,31 @@ public class CarBookingTest {
 	public void setup() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
+	}
+	
+	@Test
+	public void searchCarTest() throws InterruptedException {
+		WebElement pickUp = driver.findElement(By.xpath("//*[@id='s2id_carlocations']/a/span[1]"));
+		
+		System.out.println(pickUp.getText());
+		pickUp.click();
+		Thread.sleep(1500);
+		pickUp.sendKeys("sharjah");
+		Thread.sleep(1000);
+		pickUp.sendKeys(Keys.ENTER);
+
+	/*	WebElement pickupDate = driver.findElement(By.xpath("//*[@id='departcar']"));
+		pickupDate.clear();
+		pickupDate.click();
+		pickupDate.sendKeys("04/09/2019");
+//		pickupDate.sendKeys(Keys.ENTER);
+		
+		WebElement dropoffDate = driver.findElement(By.xpath("//*[@id='departcar']"));
+		dropoffDate.clear();
+		dropoffDate.click();
+		dropoffDate.sendKeys("07/09/2019");
+		dropoffDate.sendKeys(Keys.ENTER);  */
+
 	}
 
 }
