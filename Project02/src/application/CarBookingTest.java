ea03ec91ab85a975b8d9341267a56eb315eda25c
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-31 16:59:24 +0600

    initialize catbooking test

--- Project02/src/application/CarBookingTest.java
+++ Project02/src/application/CarBookingTest.java
@@ -1,5 +1,15 @@
 package application;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class CarBookingTest {
+	WebDriver driver;
+	JavascriptExecutor js;
+	
+	public void scrollUntilFindElement(WebElement WebElement_Path) {
+		js.executeScript("arguments[0].scrollIntoView(true);", WebElement_Path);
+	}
 
 }
