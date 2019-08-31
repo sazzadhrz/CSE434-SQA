6f236512007c83e01b90306f92d844fb8e87afc5
Sazzad Hossain <sazzadhrz@yahoo.com>
2019-08-31 17:00:19 +0600

    added invokeBrowser and setup method

--- Project02/src/application/CarBookingTest.java
+++ Project02/src/application/CarBookingTest.java
@@ -1,8 +1,13 @@
 package application;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 public class CarBookingTest {
 	WebDriver driver;
@@ -10,6 +15,22 @@ public class CarBookingTest {
 	
 	public void scrollUntilFindElement(WebElement WebElement_Path) {
 		js.executeScript("arguments[0].scrollIntoView(true);", WebElement_Path);
+	}
+	
+	@BeforeClass
+	public void invokeBrowser() {
+		System.setProperty("webdriver.chrome.driver", "F:/chromedriver_win32/chromedriver.exe");
+		
+		driver = new ChromeDriver();	
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.phptravels.net/m-cars"); 
+		js = (JavascriptExecutor)driver;
+	}
+	
+	@BeforeMethod
+	public void setup() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
 	}
 
 }
